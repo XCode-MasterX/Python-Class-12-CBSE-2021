a063be173dbc25972dde7f085e67794f87ed4bb3
DEVil Incarnate <80194462+XCode-MasterX@users.noreply.github.com>
2021-05-24 01:08:16 +1300

    Update Binary Search.py

--- Binary Search.py
+++ Binary Search.py
@@ -1,21 +1,24 @@
 ''' 
 --Sorting the list is important as Binary Search works only for sorted arrays, or lists.
---Algorithm: Selection Sort (Ascending order), if you want descending order then 
---you have to use the -- 'max' function, and also reverse the '<' to '>' and '>' to '<'
-
+--Algorithm: Shell Sort (Ascending order), if you want descending order then 
 --Big-O notation of the algorithm at hand: O(n * log n)
---It's not that bad.. So move on from here.
+--It's not that bad... So move on from here.
 '''
 # The 'sort' method. It says for itself.
 def sort(li):
-    ret_list = []
-    while li: 
-        val = min(list(li))
-        li.remove(val)
-        ret_list.append(val)
-    return ret_list
+    gap = len(li) // 2
+    while gap > 0:
+        for i in range(gap, len(li)):
+            temp = li[i]
+            j = i
+            while j >= gap and li[j - gap] > temp:
+                li[j] = li[j - gap]
+                j -= gap
 
-# Searching the element (of course)
+            li[j] = temp
+        gap //= 2
+    return li
+
 def search(li, elem):
     li = sort(li) # Sorting the list, as we can't trust someone with that shit.
     index = -1 # Initialising the index as -1 so we know that the intem was not found if the returned value is '-1'
@@ -26,13 +29,11 @@ def search(li, elem):
         if li[middle] > elem: # If the middle element is larger than the required element, then the required element is behind us.
             end = middle
             middle = (start + end) // 2
-            print("moved \'end\'")
             print(li[middle])
 
         elif li[middle] < elem: # If the middle element is smaller than the required element, then the required element is ahead of us.
             start = middle
             middle = (start + end) // 2
-            print("moved \'start\'")
             print(li[middle])
         
         if li[middle] == elem: # Checking if the current 'middle' index contains the element, if not then continue on with your day.
@@ -43,5 +44,8 @@ def search(li, elem):
 # Main Program Starts
 l = list(map(int, input("Enter your input: ").strip().split())) # Gotta take the input. Can't work without that you know.
 elem = int(input("Enter element to look for: ")) # Taking the element of our concern.
-print(search(l, elem))
+print(sort(l))
+print("Index: ", search(l, elem))
 # Main Program Ends
+
+# PROGRAM BY 'JAMES COLLINS'
