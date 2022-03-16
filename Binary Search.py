da55d41e5a2365226df54fd164e21be2e19b4485
DEVil Incarnate <80194462+XCode-MasterX@users.noreply.github.com>
2022-03-16 20:51:01 +0530

    Update Binary Search.py

--- Binary Search.py
+++ Binary Search.py
@@ -1,26 +1,5 @@
-''' 
---Sorting the list is important as Binary Search works only for sorted arrays, or lists.
---Algorithm: Shell Sort (Ascending order), if you want descending order then 
---Big-O notation of the algorithm at hand: O(n * log n)
---It's not that bad... So move on from here.
-'''
-# The 'sort' method. It says for itself.
-def sort(li):
-    gap = len(li) // 2
-    while gap > 0:
-        for i in range(gap, len(li)):
-            temp = li[i]
-            j = i
-            while j >= gap and li[j - gap] > temp:
-                li[j] = li[j - gap]
-                j -= gap
-
-            li[j] = temp
-        gap //= 2
-    return li
-
+#There's one thing. The list needs to be sorted before hand.
 def search(li, elem):
-    li = sort(li) # Sorting the list, as we can't trust someone with that shit.
     index = -1 # Initialising the index as -1 so we know that the intem was not found if the returned value is '-1'
     start, end = 0, len(li) # Initialising the starting point and the ending point.
     middle = (start + end) // 2 # The middle element of the list/array. (Don't care what you call it.)
